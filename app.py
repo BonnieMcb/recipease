0d2666deb153ce51232a2482ef01c20f16f167f0
BonnieMcb <bonniemcb@gmail.com>
2021-01-03 00:19:12 +0000

    Change safe-search toggle to OFF by default and hide login/register hyperlinks on home page if logged in

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    user = session["user"]
+
+    return render_template("index.html", user=user)
 
 
 def get_filters():
@@ -133,6 +135,9 @@ def register():
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
+        # default to safe search being on
+        session["safe_search"] = True
+
         flash("Registration successfull")
         return redirect(url_for("recipes", username=session["user"]))
 
@@ -157,6 +162,8 @@ def login():
             if check_password_hash(existing_user["password"],
                                    request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
+                # default to safe search being on
+                session["safe_search"] = True
                 flash("Welcome, {}!".format(request.form.get("username")))
                 return redirect(url_for("recipes"))
             else:
